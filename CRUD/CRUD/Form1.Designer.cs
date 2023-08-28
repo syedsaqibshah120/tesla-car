7e9c5a2b81cc25309a81de29d2a5fdb6e4203827
Saqib Shah <143094124+syedsaqibshah120@users.noreply.github.com>
2023-08-28 13:00:27 +0500

    commit
    27 aug

---
+++ CRUD/CRUD/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IDTb = new System.Windows.Forms.TextBox();
+            this.NameTb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SalaryTb = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.InsertBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.ViewBtn = new System.Windows.Forms.Button();
+            this.ResetBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GenderCombo = new System.Windows.Forms.ComboBox();
+            this.DCombo = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.searchBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // IDTb
+            // 
+            this.IDTb.Location = new System.Drawing.Point(40, 31);
+            this.IDTb.Name = "IDTb";
+            this.IDTb.Size = new System.Drawing.Size(124, 20);
+            this.IDTb.TabIndex = 0;
+            // 
+            // NameTb
+            // 
+            this.NameTb.Location = new System.Drawing.Point(303, 34);
+            this.NameTb.Name = "NameTb";
+            this.NameTb.Size = new System.Drawing.Size(124, 20);
+            this.NameTb.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(249, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(487, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Age";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(207, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Designation";
+            // 
+            // SalaryTb
+            // 
+            this.SalaryTb.Location = new System.Drawing.Point(551, 98);
+            this.SalaryTb.Name = "SalaryTb";
+            this.SalaryTb.Size = new System.Drawing.Size(124, 20);
+            this.SalaryTb.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(489, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Salary ";
+            // 
+            // InsertBtn
+            // 
+            this.InsertBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.InsertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsertBtn.Location = new System.Drawing.Point(137, 157);
+            this.InsertBtn.Name = "InsertBtn";
+            this.InsertBtn.Size = new System.Drawing.Size(101, 58);
+            this.InsertBtn.TabIndex = 6;
+            this.InsertBtn.Text = "&INSERT";
+            this.InsertBtn.UseVisualStyleBackColor = false;
+            this.InsertBtn.Click += new System.EventHandler(this.InsertBtn_Click);
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.UpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateBtn.Location = new System.Drawing.Point(244, 157);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(101, 58);
+            this.UpdateBtn.TabIndex = 7;
+            this.UpdateBtn.Text = "&UPDATE";
+            this.UpdateBtn.UseVisualStyleBackColor = false;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.BackColor = System.Drawing.Color.Red;
+            this.DeleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBtn.Location = new System.Drawing.Point(574, 157);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(101, 58);
+            this.DeleteBtn.TabIndex = 8;
+            this.DeleteBtn.Text = "&DELETE";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // ViewBtn
+            // 
+            this.ViewBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ViewBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewBtn.Location = new System.Drawing.Point(30, 157);
+            this.ViewBtn.Name = "ViewBtn";
+            this.ViewBtn.Size = new System.Drawing.Size(101, 58);
+            this.ViewBtn.TabIndex = 9;
+            this.ViewBtn.Text = "&VIEW";
+            this.ViewBtn.UseVisualStyleBackColor = false;
+            this.ViewBtn.Click += new System.EventHandler(this.ViewBtn_Click);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ResetBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetBtn.Location = new System.Drawing.Point(688, 124);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Size = new System.Drawing.Size(99, 41);
+            this.ResetBtn.TabIndex = 10;
+            this.ResetBtn.Text = "&RESET";
+            this.ResetBtn.UseVisualStyleBackColor = false;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 221);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(660, 217);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // GenderCombo
+            // 
+            this.GenderCombo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.GenderCombo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.GenderCombo.FormattingEnabled = true;
+            this.GenderCombo.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderCombo.Location = new System.Drawing.Point(554, 33);
+            this.GenderCombo.Name = "GenderCombo";
+            this.GenderCombo.Size = new System.Drawing.Size(121, 21);
+            this.GenderCombo.TabIndex = 2;
+            // 
+            // DCombo
+            // 
+            this.DCombo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.DCombo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.DCombo.FormattingEnabled = true;
+            this.DCombo.Items.AddRange(new object[] {
+            "Director",
+            "Dy Director",
+            "Manager",
+            "Accountant",
+            "IT Manager",
+            "Asstt Manager",
+            "DEO"});
+            this.DCombo.Location = new System.Drawing.Point(303, 99);
+            this.DCombo.Name = "DCombo";
+            this.DCombo.Size = new System.Drawing.Size(121, 21);
+            this.DCombo.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(53, 94);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBtn.Location = new System.Drawing.Point(687, 33);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(100, 71);
+            this.searchBtn.TabIndex = 6;
+            this.searchBtn.Text = "&Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.DCombo);
+            this.Controls.Add(this.GenderCombo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ResetBtn);
+            this.Controls.Add(this.ViewBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.InsertBtn);
+            this.Controls.Add(this.SalaryTb);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IDTb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IDTb;
+        private System.Windows.Forms.TextBox NameTb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox SalaryTb;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button InsertBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button ViewBtn;
+        private System.Windows.Forms.Button ResetBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox GenderCombo;
+        private System.Windows.Forms.ComboBox DCombo;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button searchBtn;
+    }
+}
+
---
+++ CRUD/CRUD/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertBtn_Click(object sender, EventArgs e)
+        {
+            // Validate inputs
+            if (string.IsNullOrEmpty(IDTb.Text) || string.IsNullOrEmpty(NameTb.Text)
+                || GenderCombo.SelectedItem == null || DCombo.SelectedItem == null || string.IsNullOrEmpty(SalaryTb.Text))
+            {
+                MessageBox.Show("Please fill in all the fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if the ID already exists
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                string query2 = "select * from employee where id = @id";
+                SqlCommand cmd2 = new SqlCommand(query2, con);
+                cmd2.Parameters.AddWithValue("@id", IDTb.Text);
+                SqlDataReader dr = cmd2.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    MessageBox.Show(IDTb.Text + "ID already exists !!", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                dr.Close();
+
+                // Insert the new record
+                string query = "insert into EMPLOYEE (id, name, gender, age, designation, Salary) values (@id,@name, @gender, @age, @designation, @Salary)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", IDTb.Text);
+                cmd.Parameters.AddWithValue("@name", NameTb.Text);
+                cmd.Parameters.AddWithValue("@gender", GenderCombo.SelectedItem);
+                cmd.Parameters.AddWithValue("@age", numericUpDown1.Value);
+                cmd.Parameters.AddWithValue("@designation", DCombo.SelectedItem);
+                cmd.Parameters.AddWithValue("@Salary", SalaryTb.Text);
+
+                int a = cmd.ExecuteNonQuery();
+                if (a > 0)
+                {
+                    MessageBox.Show("Inserted Successfully !!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BindGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Insertion Fail !!", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            resetcontrols();
+
+        }
+     
+        void BindGridView()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from Employee";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable data= new DataTable();
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+                    
+        }
+
+        private void ViewBtn_Click(object sender, EventArgs e)
+        {
+            BindGridView();
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            // for update
+          
+            string query = "update employee set id= @id,  gender = @gender, designation = @designation,salary= @salary where id= @id";
+
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", IDTb.Text);
+          
+            cmd.Parameters.AddWithValue("@gender", GenderCombo.SelectedItem);
+            cmd.Parameters.AddWithValue("@age", numericUpDown1.Value);
+            cmd.Parameters.AddWithValue("@designation", DCombo.SelectedItem);
+            cmd.Parameters.AddWithValue("@Salary", SalaryTb.Text);
+
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Updated Successfully !!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+            }
+            else
+            {
+                MessageBox.Show("Update Fail !!", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            con.Close();
+
+            resetcontrols();
+
+
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                IDTb.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                 NameTb.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                 GenderCombo.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                 numericUpDown1.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                 DCombo.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+                 SalaryTb.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            // for Delete
+
+            SqlConnection con = new SqlConnection(cs);
+            string query = "delete from employee where id = @id";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", IDTb.Text);
+           
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Delete Successfully !!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindGridView();
+            }
+            else
+            {
+                MessageBox.Show("Delete Fail !!", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            con.Close();
+            resetcontrols();
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            resetcontrols();
+        }
+        void resetcontrols()
+        {
+            IDTb.Clear();
+            NameTb.Clear();
+            GenderCombo.SelectedItem = null;
+            numericUpDown1.Value = 20;
+            DCombo.SelectedItem = null;
+            SalaryTb.Clear();
+            IDTb.Focus();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+
+            string searchTerm = searchBtn.Text.Trim();
+
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                MessageBox.Show("Please enter a search term", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //else
+            //{
+            //    MessageBox.Show("Please insert data");
+            //}
+               else
+            {
+                MessageBox.Show("Please insert data");
+            }
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                string query = "SELECT * FROM Employee WHERE id LIKE @searchTerm OR name LIKE @searchTerm OR Salary LIKE @searchTerm";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@searchTerm", "%" + searchTerm + "%");
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable data = new DataTable();
+                sda.Fill(data);
+                dataGridView1.DataSource = data;
+            }
+
+        }
+    }
+}
